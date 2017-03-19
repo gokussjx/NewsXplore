5ace4c840aeefa477f363395bfd84dc0e5485148
Bidyut Mukherjee <gokussjx@gmail.com>
2017-03-19 15:23:24 -0500

    Initial interface elements

---
+++ NewsXplore/ReportsListTableViewCell.swift
@@ -0,0 +1,24 @@
+//
+//  ReportsListTableViewCell.swift
+//  NewsXplore
+//
+//  Created by Bidyut Mukherjee on 3/18/17.
+//  Copyright © 2017 NewsXplore. All rights reserved.
+//
+
+import UIKit
+
+class ReportsListTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+    
+}
