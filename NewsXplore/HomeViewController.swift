9eeaa824f4c42b964006e1f6be54904a3a6f0c49
Bidyut Mukherjee <gokussjx@gmail.com>
2017-03-24 11:31:11 -0500

    Storyboard wip

--- NewsXplore/HomeViewController.swift
+++ NewsXplore/HomeViewController.swift
@@ -8,18 +8,22 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class HomeViewController: UIViewController {
 
+    @IBOutlet weak var analyzeButton: UIButton!
+    @IBOutlet weak var pasteButton: UIButton!
+    @IBOutlet weak var clearButton: UIButton!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        
+        analyzeButton.layer.cornerRadius = 7
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-
-
 }
 
