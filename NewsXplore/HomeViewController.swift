5913176c2b9880d84da59085efaca036080a4ec7
Bidyut Mukherjee <gokussjx@gmail.com>
2017-04-04 23:03:06 -0500

    1. Add Analyze button action. 2. Add sample API call and parsing

--- NewsXplore/HomeViewController.swift
+++ NewsXplore/HomeViewController.swift
@@ -7,12 +7,17 @@
 //
 
 import UIKit
+import Alamofire
 
 class HomeViewController: UIViewController {
 
     @IBOutlet weak var analyzeButton: UIButton!
     @IBOutlet weak var pasteButton: UIButton!
     @IBOutlet weak var clearButton: UIButton!
+    
+    // Sample URL. 
+    // TODO: Change to dev.newsxplore.com
+    let baseUrl = "https://jsonplaceholder.typicode.com/posts/1"
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -24,6 +29,35 @@ class HomeViewController: UIViewController {
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
+    }
+    
+    @IBAction func analyzeButton(_ sender: UIButton) {
+        Alamofire.request(baseUrl).responseJSON { response in
+            
+//            print("Request: ", terminator: "")
+//            print(response.request ?? "Request not found")  // original URL request
+//            
+//            print("Response: ", terminator: "")
+//            print(response.response ?? "Response not found")// HTTP URL response
+//            
+//            print("Data: ", terminator: "")
+//            print(response.data ?? "Data not found")        // server data
+//            
+//            print("Result: ", terminator: "")
+//            print(response.result)    // result of response serialization
+            
+            if let json = response.result.value as? [String : Any] {
+                let title = json["title"] as? String
+                let userId = json["userId"] as? Int
+                let id = json["id"] as? Int
+                let body = json["body"] as? String
+                
+                print("Title: \(title!)")
+                print("UserID: \(userId!)")
+                print("ID: \(id!)")
+                print("Body: \(body!)")
+            }
+        }
     }
 }
 
